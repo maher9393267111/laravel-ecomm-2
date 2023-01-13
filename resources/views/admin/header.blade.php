c0ab43754ac80d78725fec137f3c7a97b0823a12
maher9393267111 <89878167+maher9393267111@users.noreply.github.com>
2023-01-14 00:30:02 +0300

    admin home page sections

---
+++ resources/views/admin/header.blade.php
@@ -0,0 +1,27 @@
+{{-- <div class="container-fluid page-body-wrapper"> --}}
+    <!-- partial:partials/_navbar.html -->
+
+    <nav class="navbar p-0 fixed-top d-flex flex-row">
+        <div class="navbar-brand-wrapper d-flex d-lg-none align-items-center justify-content-center">
+            <a class="navbar-brand brand-logo-mini" href="{{ url('/redirect') }}"><img src="images/g.jpg"
+                    alt="logo" />12</a>
+        </div>
+        <div class="navbar-menu-wrapper flex-grow d-flex align-items-stretch">
+            <button class="navbar-toggler navbar-toggler align-self-center" type="button" data-toggle="minimize">
+                <span class="mdi mdi-menu"></span>
+            </button>
+
+            <ul class="navbar-nav navbar-nav-right">
+                <li class="nav-item dropdown d-none d-lg-block">
+
+
+
+
+                <li style="max-height: 60px;width:0%">
+                    <x-app-layout>
+
+                    </x-app-layout>
+                </li>
+            </ul>
+        </div>
+    </nav>
--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -12,7 +12,7 @@
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                    <x-jet-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
+                    <x-jet-nav-link href="{{ url('/redirect') }}" :active="request()->routeIs('/redirect')">
                         {{ __('Dashboard') }}
                     </x-jet-nav-link>
                 </div>
